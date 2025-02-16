ac4155af1d539d2e89e9a9e76314e4d8e11f4e27
julian <akueson85@gmail.com>
2025-02-16 21:02:46 +0100

    last version

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ st.write("Remplissez les caractéristiques du joueur et obtenez une prédiction 
 age = st.slider("Age", 16, 38, 25)
 height = st.slider("Height (Taille en cm)", 140, 220, 170)
 weight = st.slider("Weight (Poids en kg)", 40, 120, 70)
-potential = st.slider("Potential (Potentiel)", 40, 99, 85)
+#potential = st.slider("Potential (Potentiel)", 40, 99, 85)
 pac = st.slider("PAC (Vitesse)", 30, 99, 50)
 sho = st.slider("SHO (Tir)", 30, 99, 50)
 pas = st.slider("PAS (Passe)", 30, 99, 50)
@@ -26,9 +26,10 @@ defense = st.slider("DEF (Défense)", 30, 99, 50)
 phy = st.slider("PHY (Physique)", 30, 99, 50)
 weak_foot = st.slider("Weak Foot (Étoiles)", 1, 5, 3)
 skill_moves = st.slider("Skill Moves (Étoiles)", 1, 5, 3)
+#value_eur = st.slider("Value (Valeur en €)", 0, 200_000_000, 10_000_000)
 
 # Transformer les inputs en un tableau numpy
-new_player = np.array([[age,height, weight, potential, pac, sho, pas, dri, defense, phy, weak_foot, skill_moves]])
+new_player = np.array([[age,height, weight, pac, sho, pas, dri, defense, phy, weak_foot, skill_moves]])
 
 # Normalisation des données du joueur
 new_player_scaled = scaler.transform(new_player)
